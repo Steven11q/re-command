14f0439fc19ecc9b7649dd2276f607350b292dc7
Steven11q <70647969+Steven11q@users.noreply.github.com>
2025-09-02 15:34:17 -0400

    Update re-command.py

--- re-command.py
+++ re-command.py
@@ -3,6 +3,11 @@
 import os
 import navidrome_api
 import importlib
+import subprocess
+
+
+
+
 
 def main():
     """Main function to run the Navidrome recommendation script."""
@@ -16,18 +21,26 @@ def main():
     # Import config *after* the setup
     import config
     import listenbrainz_api
-    import lastfm_api
+    #import lastfm_api
 
     # Reload the config module
     importlib.reload(config)
 
+    folder = config.MUSIC_LIBRARY_PATH
+
+    #this is a bad way to do this but im too tired to do this the proper way by importing beet
+    # if you want, you can fix it
+    subprocess.run(["powershell", "beet import {}".format(folder)])
+
     # Check if the ListenBrainz playlist has changed
-    if listenbrainz_api.has_playlist_changed():
+    #listenbrainz_api.has_playlist_changed()
+    if True:
         # Download and tag new songs from ListenBrainz using deemix
         listenbrainz_api.download_new_playlist_songs_deemix()
     else:
         print("ListenBrainz playlist has not changed. Skipping download.")
 
+    # Last.fm authentication and download
 
     # Parse Navidrome library and provide feedback to ListenBrainz
     salt, token = navidrome_api.get_navidrome_auth_params()
